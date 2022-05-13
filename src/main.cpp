a852afa0382ea3f70d2c9754455050c2e4ecdb21
Vikas Lamba <vikas13jun@gmail.com>
2022-05-13 17:38:08 +0200

    OTA fixes

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 // #include <WiFiUdp.h>
-#include "wifi_events.h"
+// #include "wifi_events.h"
 #include <ArduinoWebsockets.h>
 
 #include <Preferences.h>
@@ -333,7 +333,7 @@ void initialise_wifi(void *params)
 
   while (1)
   {
-
+    if (ota_in_progress > 0) break;
     if (wifi_state == InIt)
     {
       WiFi.setSleep(false);
@@ -462,11 +462,14 @@ void update_websocket(void *params)
   vTaskDelete(NULL);
 }
 
+void(* reset_device) (void) = 0;//declare reset function at address 0
+
 void setup()
 {
   dev_config = DevConfig(&preferences);
   server_sync = ServerSync(&dev_config);
   meter = Meter(&dev_config);
+  ota_in_progress = 0;
 
 #ifdef DHT_ENABLED
   dht.begin();
@@ -532,46 +535,61 @@ void setup()
   ArduinoOTA.setPassword(dev_config.get_ota_admin_pass());
 
   ArduinoOTA
-      .onStart([]()
-               {
-                 ota_in_progress = 1;
-                 String type;
-                 if (ArduinoOTA.getCommand() == U_FLASH)
-                   type = "sketch";
-                 else // U_SPIFFS
-                   type = "filesystem";
-                 // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-                 Serial.println("Start updating " + type);
-               })
-      .onEnd([]()
-            { 
-              ota_in_progress = 0;
-              Serial.println("\nEnd");
-            })
-      .onProgress([](unsigned int progress, unsigned int total)
-                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-      .onError([](ota_error_t error)
-               {
-                 ota_in_progress = 0;
-                 Serial.printf("Error[%u]: ", error);
-                 if (error == OTA_AUTH_ERROR)
-                   Serial.println("Auth Failed");
-                 else if (error == OTA_BEGIN_ERROR)
-                   Serial.println("Begin Failed");
-                 else if (error == OTA_CONNECT_ERROR)
-                   Serial.println("Connect Failed");
-                 else if (error == OTA_RECEIVE_ERROR)
-                   Serial.println("Receive Failed");
-                 else if (error == OTA_END_ERROR)
-                   Serial.println("End Failed");
-               });
+      .onStart([]() {
+        ota_in_progress = 1;
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        // Serial.println("Start updating " + type);
+#ifdef LCD_ENABLED
+      lcd.display_message_lcd(0, "Starting OTA", 1);
+#endif
+      })
+      .onEnd([]() { 
+        ota_in_progress = 0;
+        // Serial.println("\nEnd");
+#ifdef LCD_ENABLED
+      lcd.display_message_lcd(0, "Finished OTA", 1);
+#endif
+      reset_device();
+      })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        ota_in_progress = (progress / (total / 100));
+        // Serial.printf("Progress: %u%%\r", ota_in_progress);
+#ifdef LCD_ENABLED
+      if (ota_in_progress % 5 == 0) {
+        sprintf(ui_buffer, "OTA: %hu%", ota_in_progress);
+        lcd.display_message_lcd(0, ui_buffer, 1);
+      }
+#endif
+      })
+      .onError([](ota_error_t error) {
+        ota_in_progress = 0;
+        // Serial.printf("Error[%u]: ", error);
+        // if (error == OTA_AUTH_ERROR)
+        //   Serial.println("Auth Failed");
+        // else if (error == OTA_BEGIN_ERROR)
+        //   Serial.println("Begin Failed");
+        // else if (error == OTA_CONNECT_ERROR)
+        //   Serial.println("Connect Failed");
+        // else if (error == OTA_RECEIVE_ERROR)
+        //   Serial.println("Receive Failed");
+        // else if (error == OTA_END_ERROR)
+        //   Serial.println("End Failed");
+#ifdef LCD_ENABLED
+      sprintf(ui_buffer, "Failed: %u", error);
+      lcd.display_message_lcd(0, ui_buffer, 1);
+#endif
+      reset_device();
+      });
 
       ArduinoOTA.begin();
 }
 
 void loop()
 {
-  if (wifi_state >= ConnectedAP) {
-    ArduinoOTA.handle();
-  }
+  ArduinoOTA.handle();
 }
